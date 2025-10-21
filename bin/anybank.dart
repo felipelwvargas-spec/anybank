e260093120658dd546f53d24115d204ec1031365
Felipe Vargas <felipelwvargas@gmail.com>
2025-10-21 09:34:04 -0300

    Class conta

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,5 +1,17 @@
-import 'package:anybank/anybank.dart' as anybank;
+import 'package:anybank/conta.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${anybank.calculate()}!');
+void main() {
+  Conta contaMatheus = Conta("Matheus", 1000);
+  Conta contaRoberta = Conta("Roberta", 2000);
+
+  List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
+
+  for (Conta conta in contas){
+    conta.imprimeSaldo();
+  }
+  contaRoberta.receber(3000);
+  contaMatheus.receber(500);
+  contaMatheus.enviar(200);
+  
+
 }
---
+++ lib/conta.dart
@@ -0,0 +1,22 @@
+class Conta {
+  String titular;
+  double _saldo;
+  Conta(this.titular, this._saldo);
+
+  void receber(double valor) {
+    _saldo += valor;
+    imprimeSaldo();
+  }
+
+  void enviar(double valor) {
+    if (_saldo >= valor) {
+      _saldo -= valor;
+      imprimeSaldo();
+    } else {
+      print("Saldo insuficiente!");
+    }
+  }
+  void imprimeSaldo(){
+    print("O saldo atual de $titular, é: R\$$_saldo");
+  }
+}
